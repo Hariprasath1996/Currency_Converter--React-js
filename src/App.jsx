f1fd56c6f25f715eced017c3a084f17186449b99
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-08 22:52:36 +0530

    use axios method

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
 
 
   const[amount,setAmount]=useState(1);
-  const [fromCurrency,setFromCurrency]=useState("");
-  const [toCurrency,setToCurrency]=useState("");
-  const [convertedAmount,setConvertedAmount]=(null);
+  const [fromCurrency,setFromCurrency]=useState("USD");
+  const [toCurrency,setToCurrency]=useState("INR");
+  const [convertedAmount,setConvertedAmount]=("");
+  // create one more state variable for ,getting current exchange rate values 
+  const [exchangeRate,setExchangeRate]=useState(null);
 
+  // create a component 
+useEffect(
+  ()=>{
+const convert = async ()=>{
+  try {
+    let url =`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`
+    const res = await axios.get(url);
+    console.log(res);
+    setExchangeRate(res.data.rates[toCurrency])
+    
+  } catch (error) {
+    console.error ("Error Fetching Exchange Data :" , error)
+  }
+  convert()
+}
+});
+// create new component for get  amount  value in their input 
+const eventAmountHandler=(e)=>{
+const value = parseFloat(e.target.value);
+setAmount(isNaN (value) ? 0 : value)
+}
+// create to components here , one for fromCurrency another one toCurrency
+
+const getFromCurrency=(e)=>{
+  setFromCurrency(e.target.value)
+}
+
+const getToCurrency=(e)=>{
+  setToCurrency(e.target.value)
+}
   return (<>
     <div className="currency-convertor">
       <div className="section-1">
@@ -19,11 +53,11 @@ const App = () => {
         </div>
         <div className="input-container">
           <label className="amount" htmlFor="amount" >Amount :</label>
-          <input value={amount} type="text" id="amount" />
+          <input onChange={eventAmountHandler} value={amount} type="text" id="amount" />
         </div>
         <div className="input-container-one">
           <label className="currency-from" htmlFor="currency-from">From Currency :</label>
-          <select id="currency-from" value={fromCurrency}>
+          <select onChange={getFromCurrency} id="currency-from" value={fromCurrency}>
             <option value="USD">USD-UNITED STATUS DOLLAR</option>
             <option value="EUR">EUR-EURO</option>
             <option value="GBP">GBP-BRITISH POUND STERLING</option>
@@ -38,7 +72,7 @@ const App = () => {
         </div>
         <div className="input-container-two">
           <label className="currency-to" htmlFor="currency-to">To Currency :</label>
-          <select id="currency-to" value={toCurrency}>
+          <select onChange={getToCurrency} id="currency-to" value={toCurrency}>
             <option value="USD">USD-UNITED STATUS DOLLAR</option>
             <option value="EUR">EUR-EURO</option>
             <option value="GBP">GBP-BRITISH POUND STERLING</option>
